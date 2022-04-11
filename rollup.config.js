a845787d6a67a87e1bef3f34f9c2884d8d7bea6c
Baptiste.z <zellerbaptiste@gmail.com>
2022-04-11 16:00:26 +0200

    feat(rollup): Add CJS output config (#6)

--- rollup.config.js
+++ rollup.config.js
@@ -81,6 +81,52 @@ export default [
     },
   },
   {
+    input: "src/index.js",
+    output: {
+      sourcemap: true,
+      format: "cjs",
+      file: "public/build/svelte-editorjs.js",
+    },
+    plugins: [
+      svelte({
+        preprocess: sveltePreprocess(),
+        compilerOptions: {
+          // enable run-time checks when not in production
+          dev: !production,
+        },
+      }),
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css({ output: "svelte-editorjs.css" }),
+
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ["svelte"],
+      }),
+      commonjs(),
+
+      // In dev mode, call `npm run start` once
+      // the bundle has been generated
+      !production && serve(),
+
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload("public"),
+
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser(),
+    ],
+    watch: {
+      clearScreen: false,
+    },
+  },
+  {
     input: "public/example_configuration.js",
     output: {
       format: "iife",
